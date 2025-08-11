88e2ba1d2665ac9b125bd5c00883cb9f4468085f
Bruno Feliciano Alves <gabriel@rwstudiobruno.feliciano@gmail.com>
2025-08-11 07:13:00 -0300

    Configirado Banco de dados Postgree

--- src/main/java/projeto_spring_boot/projeto_spring/controller/ServicosController.java
+++ src/main/java/projeto_spring_boot/projeto_spring/controller/ServicosController.java
@@ -1,28 +1,21 @@
 package projeto_spring_boot.projeto_spring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 //@RequestMapping("/private/servicos")
 public class ServicosController {
 
-    @GetMapping("/videos")
-    public String videos() {
-        return "private/servicos/videos";
-    }
-
-    @GetMapping("/mobile")
-    public String mobile() {
-        return "private/servicos/mobile";
-    }
-
-    @GetMapping("/consultoria")
-    public String consultoria() {
-        return "private/servicos/consultoria";
-    }
-    
-
+//	private final FilmeRepository filmeRepository;
+//
+//    public FilmeController(FilmeRepository filmeRepository) {
+//        this.filmeRepository = filmeRepository;
+//    }
+//    
+//    @GetMapping("/novo")
+//    public String novoFilmeForm(Model model) {
+//        model.addAttribute("filme", new Filme());
+//        return "private/servicos/form-filmes";
+//    }
 
 }
